afa967d0fa1c74dd47b73f01cc2e1c593269317d
Ayobami6 <ayobamidele006@gmail.com>
2024-10-27 13:03:15 +0100

    feat: Create config file if not present
    Initializes a default `config.yaml` file if one doesn't exist upon application startup. This ensures a smoother user experience by preventing potential errors when the config file is expected but not found.

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/Ayobami6/todo_cli/cmd"
 	_ "github.com/joho/godotenv/autoload"
@@ -13,9 +14,18 @@ func main() {
 	viper.SetDefault("passcode", "1234")
 
 	viper.SetConfigName("config") // Config file name (without extension)
-	viper.SetConfigType("json")   // Config file type (could be json, yaml, etc.)
+	viper.SetConfigType("yaml")   // Config file type (could be json, yaml, etc.)
 	viper.AddConfigPath(".")
-	err := viper.ReadInConfig()
+	// check if config file exists, if not create
+	_, err := os.Stat("config.yaml")
+	// if file does not exist, create it
+	if os.IsNotExist(err) {
+		_, err := os.Create("config.yaml")
+		if err != nil {
+			panic(err)
+		}
+	}
+	err = viper.ReadInConfig()
 	if err != nil {
 		panic(fmt.Errorf("fatal error config file: %w", err))
 	}
